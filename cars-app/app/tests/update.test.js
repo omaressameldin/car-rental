cedeadddb3a71392886600cf371494f93c62a4ec
Omar Essam <o.essameldin@gmail.com>
2018-09-08 18:21:02 +0200

    [tests] adjust user update test to update all attributes

---
+++ cars-app/app/tests/update.test.js
@@ -0,0 +1,46 @@
+import test                from 'ava';
+import { Car }            from '../car.js';
+import { HelperFunctions } from './helper_functions.js'
+
+test('update single car attribute', async t => {
+  const car           = new Car(HelperFunctions.carParams());
+  const {updatedAt}   = car;
+  const updatedParams = {model: "VW"};
+  const updatedCar    = await new Promise( (resolve) => setTimeout(() => resolve(Car.updateCar(car._id, updatedParams)), 100));
+
+  t.deepEqual({...updatedCar}, {...car, ...updatedParams});
+  t.not(car.updatedAt, updatedAt);
+  t.deepEqual(Object.keys(car).sort(), HelperFunctions.carAttributes().sort());
+});
+
+test('update multiple car attribute', async t => {
+  const car           = new Car(HelperFunctions.carParams());
+  const {updatedAt}   = car
+  const updatedParams = {model: "VW", color: "BLACK"};
+  const updatedCar    = await new Promise( (resolve) => setTimeout(() => resolve(Car.updateCar(car._id, updatedParams)), 100));
+
+  t.deepEqual({...updatedCar}, {...car, ...updatedParams});
+  t.not(car.updatedAt, updatedAt);
+  t.deepEqual(Object.keys(car).sort(), HelperFunctions.carAttributes().sort());
+});
+
+test('update non existing car', t => {
+  const error = t.throws(() => {
+    Car.updateCar("asd", {});
+  });
+
+  t.deepEqual(HelperFunctions.sanitizeError(error), ['can not find car with that id']);
+});
+
+test('create errors work on update', t => {
+  const car        = new Car(HelperFunctions.carParams());
+  const error = t.throws(() => {
+    Car.updateCar(car._id, {name: "", age: -1, gender: "MO"});
+  });
+
+  t.deepEqual(HelperFunctions.sanitizeError(error).sort(), ['Name should only contain letters', 
+                                                            'Name length has to be more than or equal to 3', 
+                                                            'Age must be between 16 and 65', 
+                                                            'Gender must be either M or F'].sort());
+});
+
