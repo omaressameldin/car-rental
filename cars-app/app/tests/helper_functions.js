6b41bb2e40b72f299cfb7117ac8ca7c2e6897634
Omar Essam <o.essameldin@gmail.com>
2018-09-08 18:18:06 +0200

    [tests] add car tests helper functions

---
+++ cars-app/app/tests/helper_functions.js
@@ -0,0 +1,19 @@
+module.exports.HelperFunctions = {
+  carParams: (params = {}) => {
+    return {
+      model:              "BMW",
+      type:               "CABRIOLET",
+      color:              "BLUE",
+      infotainmentSystem: "BLUETOOTH",
+      engineNumber:       `${Math.random().toString(36).substr(2,1)}-${Math.random().toString(36).substr(2, 6)}`,
+      location:           {x: 14, y: 27},
+      isLeatherInterior:  true,
+      ...params};
+  },
+  sanitizeError: (error) => {
+    return error.message.split(",")
+  },
+  carAttributes: () => {
+    return ['model', 'type', 'color', 'infotainmentSystem', 'engineNumber', 'location', 'isLeatherInterior', 'createdAt', 'updatedAt', '_id'];
+  }
+}
