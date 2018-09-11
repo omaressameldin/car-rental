233fa07d75d9de6765a8aa1fb7401819e951f239
Omar Essam <o.essameldin@gmail.com>
2018-09-11 02:53:32 +0200

    [tests] add a test for deleting a user with a demand

--- users-app/app/tests/delete.test.js
+++ users-app/app/tests/delete.test.js
@@ -1,17 +1,30 @@
 import test                from 'ava';
+const axios =              require('axios');
 import { User }            from '../user.js';
 import { HelperFunctions } from './helper_functions.js'
 
-test('delete user', t => {
+test('delete user', async t => {
   const user = new User(HelperFunctions.userParams());
-  User.deleteUser(user._id);
-  t.deepEqual(User.all(), []);
+  await User.deleteUser(user._id);
+  t.deepEqual(User.all().filter((({_id}) => _id === user._id)), []);
 });
 
-test('delete non existing user', t => {
-  const error = t.throws(() => {
-    User.deleteUser("asd");
+test('delete non existing user', async t => {
+  const error = await t.throwsAsync(async () => {
+    await User.deleteUser("asd");
   });
 
   t.deepEqual(HelperFunctions.sanitizeError(error), ['can not find user with that id']);
 });
+
+test('delete a user that has a demand', async t => {
+  const user   = (await axios.post('http://localhost:3000/users', {user: HelperFunctions.userParams()})).data;
+  await HelperFunctions.createDemand(user._id);  
+  
+  const error = await t.throwsAsync(async () => {
+    await User.deleteUser(user._id);
+  });
+
+  t.deepEqual(HelperFunctions.sanitizeError(error), ['can not delete a user that has a demand']);
+});
+
--- users-app/app/tests/helper_functions.js
+++ users-app/app/tests/helper_functions.js
@@ -1,3 +1,5 @@
+const axios = require('axios');
+
 module.exports.HelperFunctions = {
   userParams: (params = {}) => {
     return {name: "omar", age: 20, gender: "M", ...params};
@@ -7,5 +9,30 @@ module.exports.HelperFunctions = {
   },
   userAttributes: () => {
     return ['name', 'age', 'gender', 'createdAt', 'updatedAt', '_id'];
-  }
+  },
+  createDemand: (userID) => {
+    return axios.post('http://demands-app:3000/demands',  {
+      demand: {
+        userID: userID,
+        model: "VW",
+        color: "GREEN",
+        type: "SEDAN",
+        infotainmentSystem: "CD",
+        isLeatherInterior: true,
+        pickupLocation: {
+          xPickup: 14,
+          yPickup: 27
+        },
+        dropoffLocation: {
+          xDropoff: 14,
+          yDropoff: 27
+        },
+        pickupTime: "2018-10-11T11:30:00.000Z",
+        dropoffTime: "2018-10-31T11:30:00.000Z",
+      }
+    })
+    .then(function (response) {
+      return response.data
+    })
+  }  
 }
